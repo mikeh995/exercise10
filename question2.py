5e1cda4262f25e5c8fc0292f9d869920a672b9d1
mikeh995 <mikeh995@hotmail.co.uk>
2023-03-06 12:51:42 +0000

    Updating local project files for exercise 10

---
+++ question2.py
@@ -0,0 +1,22 @@
+supplied_pin = None
+tries = 0
+
+firstpin = input("PLEASE ENTER YOUR PIN NUMBER:")
+if firstpin == "1234":
+   tries += 1
+   print("")
+   print("PIN Attempts:", tries)
+   exit("@@@@   WELCOME   @@@@@")
+else:
+   tries +=1
+   print("")
+   print("PIN Attempts:", tries)
+
+while supplied_pin != "1234":
+   tries +=1
+   if tries == 4 :exit("FAILED TO UNLOCK! -TOO MANY FAILED ATTEMPTS")
+   print("INCORRECT PIN")
+   supplied_pin = input("Enter Your Pin:")
+   print("")
+   print("PIN Attempts:", tries)
+exit("@@@@@  WELCOME  @@@@@")
---
+++ question3.py
@@ -0,0 +1,25 @@
+from getpass import getpass
+supplied_pin = None
+tries = 0
+
+firstpin = getpass("PLEASE ENTER YOUR PIN NUMBER: ")
+
+if firstpin == "1234":
+    tries += 1
+    print("")
+    print("PIN Attempts:", tries)
+    exit("        ***WELCOME***        ")
+else:
+    tries += 1
+    print("")
+    print("PIN Attempts:", tries)
+
+while supplied_pin != "1234":
+    tries += 1
+    if tries == 4: exit("FAILED TO UNLOCK! -TOO MANY FAILED ATTEMPTS")
+    print("INCORRECT PIN")
+    supplied_pin = getpass("Enter Your Pin:")
+    print("")
+    print("PIN Attempts:", tries)
+
+exit("        ****WELCOME****        ")
